468395fa0aa36d256683c2e2e4d62d331e7b7602
Angey22 <babandval@gmail.com>
2022-11-13 15:28:52 +0200

    Added code in file '07-2-discount.js'

--- js-repeta/modul_1-1/07-2-discount.js
+++ js-repeta/modul_1-1/07-2-discount.js
@@ -16,7 +16,30 @@
 * - в результате работы скрипта вывести сообщение "Оформляем заказ на сумму [число] со скидкой [скидка]%"
 */
 
-const totalSpent = 2000;
+// 1-е условие
+let totalSpent = 2000;
 let payment = 500;
 let discount = 0;
 
+// 2-е условие
+if (totalSpent < 100) {
+console.log('У Вас еще нет партнерской скидки.');
+}
+else if (totalSpent >= 100 && totalSpent < 1000) {
+    discount = 0.02;
+    console.log('Бронзовый партнер, скидка 2%');
+} else if (totalSpent >= 1000 && totalSpent < 5000) {
+    discount = 0.05;
+    console.log('Серебрянный партнер, скидка 5%');
+} else {
+    discount = 0.1;
+    console.log('Золотой партнер, скидка 10%');
+}
+
+// 3-е условие
+payment -= payment * discount; // payment = payment - payment * discount;
+console.log(`Оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`);
+
+// Дополнительные операции
+totalSpent += payment;
+console.log(`Общая сумма потраченых средств в магазине: ${totalSpent}`);
